311a7a9fa6673379a7fcea08a99e2b81843a21d5
vgaidukov <vgaidukov@gmail.com>
2022-10-20 20:10:39 +0300

    feat: add Helmet protection

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
 
 const limiter = require('./middlewares/limiter');
 const corsHandler = require('./middlewares/corsHandler');
@@ -16,10 +17,10 @@ const app = express();
 
 mongoose.connect(DB_LINK);
 
-app.disable('x-powered-by');
 app.use(limiter);
-app.use(bodyParser.json());
+app.use(helmet());
 app.use(corsHandler);
+app.use(bodyParser.json());
 
 app.use(requestLogger);
 
