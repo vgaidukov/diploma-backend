57168daed182961036d698c2e5f3ada79c4cdafb
vgaidukov <vgaidukov@gmail.com>
2022-10-21 19:43:04 +0300

    fix: set name & email required for Celebrate validation of userInfo update request

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -19,8 +19,8 @@ const validateAuthentication = celebrate({
 
 const validateUserInfo = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    email: Joi.string().email(),
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
   }),
 });
 
