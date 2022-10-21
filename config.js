db6f7c3c8f5c42f07df22fd7a879b3d59027ecad
vgaidukov <vgaidukov@gmail.com>
2022-10-21 21:58:29 +0300

    replace database with another one named Moviesdb

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const JWT_DEV_SECRET = 'dev-secret';
-const DB_DEV_LINK = 'mongodb://localhost:27017/bitfilmsdb';
+const DB_DEV_LINK = 'mongodb://localhost:27017/moviesdb';
 
 module.exports = { JWT_DEV_SECRET, DB_DEV_LINK };
