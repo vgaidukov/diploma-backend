dda82e2739324751a483c73c242851296104e955
vgaidukov <vgaidukov@gmail.com>
2022-11-18 01:22:59 +0300

    add allowed urls

--- middlewares/corsHandler.js
+++ middlewares/corsHandler.js
@@ -1,7 +1,7 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  // 'https://vgaidukov.students.nomoredomains.icu',
-  // 'http://vgaidukov.students.nomoredomains.icu',
+  'https://movies-vg.nomoredomains.icu',
+  'http://movies-vg.nomoredomains.icu',
   'http://localhost:3000',
 ];
 
